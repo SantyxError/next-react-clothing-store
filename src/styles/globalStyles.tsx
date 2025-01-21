281bcd538aa8094ab53b5a22330fe3a85a245c89
Santiago Benito <bmsanti@hotmail.com>
2025-01-21 12:33:31 +0100

    fix: resolve styling issue

--- src/styles/globalStyles.tsx
+++ src/styles/globalStyles.tsx
@@ -1,14 +1,13 @@
 import { createGlobalStyle } from "styled-components";
-import { theme } from "../styles/theme"; //
 
 const GlobalStyles = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&family=Poppins:wght@200;400&display=swap');
 
   html,
   body {
-    background-color: ${theme.color.background};
-    color: ${theme.color.textPrimary};
-    font-family: ${theme.fontFamily.primary};
+    background-color: ${({ theme }) => theme.color.background};
+    color: ${({ theme }) => theme.color.textPrimary};
+    font-family: ${({ theme }) => theme.fontFamily.primary};
     margin: 0;
     padding: 0;
     box-sizing: border-box;
